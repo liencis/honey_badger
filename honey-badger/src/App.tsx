574bb33daeb8b1cbae1e06e22ebaa3a5c0390196
liencis <lienezegele@gmail.com>
2026-07-22 17:39:03 +0300

    Added Dificulty level to navbar

--- honey-badger/src/App.tsx
+++ honey-badger/src/App.tsx
@@ -1,84 +1,119 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import { gameSetup, Level } from './setup/grid';
-import type { CellInfo } from './setup/grid';
-import CellsGrid from './CellsGrid';
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useDeferredValue,
+} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import { gameSetup, Level } from "./setup/grid";
+import type { CellInfo } from "./setup/grid";
+import CellsGrid from "./CellsGrid";
 
-import './App.css'
-import Header from './Header'
-import VictoryCard from './VictoryCard'
-import ConfettiCard from './ConfettiCard'
-import GameOverCard from './GameOverCard'
-import BeeAttack from './BeeAttack'
+import "./App.css";
+import Header from "./Header";
+import VictoryCard from "./VictoryCard";
+import ConfettiCard from "./ConfettiCard";
+import GameOverCard from "./GameOverCard";
+import BeeAttack from "./BeeAttack";
+import Navbar from "./Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const row = 15;
-  const col = 15;
-  const [game, beePlacement] = gameSetup(row, col, Level.easy);
+  const [gameLevel, setGameLevel] = useState(Level.easy);
+  const [gameDimensions, setGameDimensions] = useState({ row: 15, col: 15 });
+  const deferredDimensions = useDeferredValue(gameDimensions);
+  const [game, beePlacement] = gameSetup(
+    gameDimensions.row,
+    gameDimensions.col,
+    gameLevel,
+  );
   const [cells, setCells] = useState<Map<number, CellInfo>>(game);
   const [gameOver, setGameOver] = useState(false);
   const [gameWon, setGameWon] = useState(false);
   const [numBees, setNumBees] = useState(beePlacement.length);
   const [score, setScore] = useState(0);
-  const [level, setLevel] = useState(Level.easy);
-
+  const [navbarExpand, setNavbarExpand] = useState(false);
 
   interface CellClickEvent extends React.MouseEvent<HTMLButtonElement> {
     target: HTMLButtonElement & { value: string };
   }
 
+  const startNewGame = () => {
+    const [newGame, newBeePlacement] = gameSetup(
+      gameDimensions.row,
+      gameDimensions.col,
+      gameLevel,
+    );
+    setCells(newGame);
+    setNumBees(newBeePlacement.length);
+    setGameOver(false);
+    setGameWon(false);
+  };
+
+  useEffect(() => {
+    startNewGame();
+  }, [gameDimensions, gameLevel]);
+
   useEffect(() => {
     if (gameWon === true) {
-      setScore(prev => {
+      setScore((prev) => {
         let numCells = cells.size;
         let multiply = 1;
-        if (level === Level.easy) {
-          multiply = 4
-        } else if (level === Level.normal) {
-          multiply = 5
-        } else if (level === Level.hard) {
-          multiply = 6
+        if (gameLevel === Level.easy) {
+          multiply = 4;
+        } else if (gameLevel === Level.normal) {
+          multiply = 5;
+        } else if (gameLevel === Level.hard) {
+          multiply = 6;
         } else {
-          multiply = 8
+          multiply = 8;
         }
-        return prev + (numCells * multiply)
-      })
+        return prev + numCells * multiply;
+      });
     }
   }, [gameWon]);
 
   return (
     <>
-      {gameWon && 
+      <Navbar
+        onMouseEnter={() => setNavbarExpand(true)}
+        onMouseLeave={() => setNavbarExpand(false)}
+        navbarExpand={navbarExpand}
+        setGameLevel={setGameLevel}
+        gameLevel={gameLevel}
+        setGameDimensions={setGameDimensions}
+        gameDimensions={gameDimensions}
+      />
+      {gameWon && (
         <div>
-          <ConfettiCard/> 
+          <ConfettiCard />
           <VictoryCard setGameWon={setGameWon} />
         </div>
-      }
-      {
-        gameOver && 
+      )}
+      {gameOver && (
         <div>
-          <BeeAttack/>
-          <GameOverCard setGameOver={setGameOver}/>
+          <BeeAttack />
+          <GameOverCard setGameOver={setGameOver} />
         </div>
-      }
+      )}
       <section id="center">
         <div className="hero">
-          <Header gameOver={gameOver}/>
+          <Header gameOver={gameOver} />
         </div>
-        <div className='dataTablo'>
-          <ul className='dataTabloList'>
+
+        <div className="dataTablo">
+          <ul className="dataTabloList">
             <li>Player: {"Player Name"}</li>
             <li>Bees: {numBees}</li>
             <li>Score: {score}</li>
           </ul>
         </div>
-        <CellsGrid 
-          cells={cells} 
-          row={row} 
-          col={col} 
-          setCell={setCells} 
+        <CellsGrid
+          cells={cells}
+          row={deferredDimensions.row}
+          col={deferredDimensions.col}
+          setCell={setCells}
           setGameOver={setGameOver}
           setGameWon={setGameWon}
           setNumBees={setNumBees}
@@ -171,7 +206,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- honey-badger/src/CellsGrid.tsx
+++ honey-badger/src/CellsGrid.tsx
@@ -1,189 +1,217 @@
 import Cell from "./Cell";
 import type { CellInfo } from "./setup/grid";
-import './App.css';
-import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
+import "./App.css";
+import {
+  memo,
+  useEffect,
+  useState,
+  type Dispatch,
+  type SetStateAction,
+} from "react";
 import { v4 as uuid } from "uuid";
 import bee from "./assets/bee.svg";
 
 interface CellsGridProps {
-    cells: Map<number, CellInfo>,
-    row: number,
-    col: number,
-    setCell: Dispatch<SetStateAction<Map<number, CellInfo>>>,
-    setGameOver: Dispatch<SetStateAction<boolean>>,
-    setGameWon: Dispatch<SetStateAction<boolean>>,
-    setNumBees: Dispatch<SetStateAction<number>>,
+  cells: Map<number, CellInfo>;
+  row: number;
+  col: number;
+  setCell: Dispatch<SetStateAction<Map<number, CellInfo>>>;
+  setGameOver: Dispatch<SetStateAction<boolean>>;
+  setGameWon: Dispatch<SetStateAction<boolean>>;
+  setNumBees: Dispatch<SetStateAction<number>>;
 }
 
-function CellsGrid ({cells, row, col, setCell, setGameOver, setGameWon, setNumBees}: CellsGridProps) {
-    const rowArray = Array.from({length: row}, (_, i) => i); // Array.from({length: row}, (_, i) => i + 1)
-    const colArray = Array.from({length: col}, (_, i) => i);
-    let numCellsPlaced = 0;
+function CellsGrid({
+  cells,
+  row,
+  col,
+  setCell,
+  setGameOver,
+  setGameWon,
+  setNumBees,
+}: CellsGridProps) {
+  const rowArray = Array.from({ length: row }, (_, i) => i); // Array.from({length: row}, (_, i) => i + 1)
+  const colArray = Array.from({ length: col }, (_, i) => i);
+  let numCellsPlaced = 0;
 
-    const [visible, setVisible] = useState(false);
-    const [points, setPoints] = useState({ x: 0, y: 0 });
-    const [cellNumber, setCellNumber] = useState(0);
+  const [visible, setVisible] = useState(false);
+  const [points, setPoints] = useState({ x: 0, y: 0 });
+  const [cellNumber, setCellNumber] = useState(0);
 
-    const [isHovered, setIsHovered] = useState(false);
-    const [isHoveredUnmark, setIsHoveredUnmark] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHoveredUnmark, setIsHoveredUnmark] = useState(false);
 
-    // Close the custom menu when clicking outside of it
-    useEffect(() => {
-        const handleClick = () => setVisible(false);
-        window.addEventListener("click", handleClick);
-        return () => window.removeEventListener("click", handleClick);
-    }, []);
+  // Close the custom menu when clicking outside of it
+  useEffect(() => {
+    const handleClick = () => setVisible(false);
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
 
-    const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault(); // Prevent default behavior (e.g., context menu on right-click)
-        setPoints({ x: e.clientX, y: e.clientY }); // get click coordinates
-        setCellNumber(Number(e.target["id"]));
-        setVisible(true); // show custom menu
-    }
+  const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Prevent default behavior (e.g., context menu on right-click)
+    setPoints({ x: e.clientX, y: e.clientY }); // get click coordinates
+    setCellNumber(Number(e.target["id"]));
+    setVisible(true); // show custom menu
+  };
 
-    const markBee = () => {
-        let cellInfo = cells.get(cellNumber);
-        if (cellInfo) {
-            if (cellInfo.open === false) {
-                cellInfo.beeMarked = true;
-                setNumBees((prevNum) => {
-                    if (prevNum > 0) {
-                    return prevNum - 1;
-                    }
-                    return 0;
-                });
-            }
-        }   
-        setCell((prevCells) => {
-            const newCells = new Map(prevCells);
-            newCells.set(Number(cellNumber), cellInfo);
-            return newCells;
+  const markBee = () => {
+    let cellInfo = cells.get(cellNumber);
+    if (cellInfo) {
+      if (cellInfo.open === false) {
+        cellInfo.beeMarked = true;
+        setNumBees((prevNum) => {
+          if (prevNum > 0) {
+            return prevNum - 1;
+          }
+          return 0;
         });
-
-        setVisible(false);
+      }
     }
+    setCell((prevCells) => {
+      const newCells = new Map(prevCells);
+      newCells.set(Number(cellNumber), cellInfo);
+      return newCells;
+    });
 
-    const unmarkBee = () => {
-        let cellInfo = cells.get(cellNumber);
-        if (cellInfo) {
-            if (cellInfo.open === false && cellInfo.beeMarked === true) {
-                cellInfo.beeMarked = false;
-                setNumBees((prevNum) => prevNum + 1);
-            }
+    setVisible(false);
+  };
+
+  const unmarkBee = () => {
+    let cellInfo = cells.get(cellNumber);
+    if (cellInfo) {
+      if (cellInfo.open === false && cellInfo.beeMarked === true) {
+        cellInfo.beeMarked = false;
+        setNumBees((prevNum) => prevNum + 1);
+      }
+    }
+    setCell((prevCells) => {
+      const newCells = new Map(prevCells);
+      newCells.set(Number(cellNumber), cellInfo);
+      return newCells;
+    });
+    setVisible(false);
+  };
+
+  const liStyle = {
+    padding: "1px",
+    ...(isHovered
+      ? {
+          backgroundColor: "#f6edc9",
+          borderRadius: "5px",
         }
-        setCell((prevCells) => {
-            const newCells = new Map(prevCells);
-            newCells.set(Number(cellNumber), cellInfo);
-            return newCells;
-        });
-        setVisible(false);
-    }
+      : {}),
+  };
 
-    const liStyle = {
-        padding: "1px",
-        ...(isHovered ? { 
-            backgroundColor: "#f6edc9",
-            borderRadius: "5px",
-        } : {}),
-    }
+  const liStyleUnmark = {
+    padding: "1px",
+    ...(isHoveredUnmark
+      ? {
+          backgroundColor: "#f6edc9",
+          borderRadius: "5px",
+        }
+      : {}),
+  };
 
-    const liStyleUnmark = {
-        padding: "1px",
-        ...(isHoveredUnmark ? { 
-            backgroundColor: "#f6edc9",
-            borderRadius: "5px",
-        } : {}),
-    }
-
-    return ( 
-        <div className="cellContainer" onContextMenu={handleRightClick}>
-            <p>Cell Number: {cellNumber}</p>
-            {/* right clickik */}
-            {visible && (
-                <ul className="custom-menu" 
-                    style={{ 
-                        top: points.y, 
-                        left: points.x,
-                        zIndex: 1000,
-                        position: "absolute",
-                        textAlign: "center",
-                        backgroundColor: "#fff",
-                        border: "1px solid #ccc",
-                        listStyle: "none",
-                        padding: "10px",
-                        margin: 0,
-                        boxShadow: "2px 2px 5px rgba(0,0,0,0.2)",
-                    }}
-                >
-                    <li onClick={() => {markBee();}}
-                        style={liStyle}
-                        onMouseEnter={() => setIsHovered(true)}
-                        onMouseLeave={() => setIsHovered(false)}
+  return (
+    <div className="cellContainer" onContextMenu={handleRightClick}>
+      <p>Cell Number: {cellNumber}</p>
+      {/* right clickik */}
+      {visible && (
+        <ul
+          className="custom-menu"
+          style={{
+            top: points.y,
+            left: points.x,
+            zIndex: 1000,
+            position: "absolute",
+            textAlign: "center",
+            backgroundColor: "#fff",
+            border: "1px solid #ccc",
+            listStyle: "none",
+            padding: "10px",
+            margin: 0,
+            boxShadow: "2px 2px 5px rgba(0,0,0,0.2)",
+          }}
+        >
+          <li
+            onClick={() => {
+              markBee();
+            }}
+            style={liStyle}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Mark:
+            <img src={bee} alt="Bee" width="30" height="30" />
+          </li>
+          <hr />
+          <li
+            onClick={() => {
+              unmarkBee();
+            }}
+            style={liStyleUnmark}
+            onMouseEnter={() => setIsHoveredUnmark(true)}
+            onMouseLeave={() => setIsHoveredUnmark(false)}
+          >
+            Unmark Bee
+          </li>
+        </ul>
+      )}
+      {rowArray.map((r) => {
+        const className = Number(r) % 2 === 0 ? "evenRow" : "oddRow";
+        return (
+          <ul key={Number(r)} className={className}>
+            {colArray.map((c) => {
+              if (className === "evenRow") {
+                if (Number(c) % 2 === 0) {
+                  return <li className="cellLi" key={uuid()}></li>;
+                } else {
+                  numCellsPlaced += 1;
+                  return (
+                    <li
+                      key={numCellsPlaced}
+                      value={numCellsPlaced}
+                      className="cellLi"
+                      id={`${numCellsPlaced}`}
                     >
-                        Mark:
-                        <img src={bee} alt="Bee" width="30" height="30"/>
+                      <Cell
+                        cellInfo={cells.get(numCellsPlaced)}
+                        setCell={setCell}
+                        setGameOver={setGameOver}
+                        setGameWon={setGameWon}
+                      />
                     </li>
-                    <hr/>
-                    <li onClick={() => {unmarkBee();}}
-                        style={liStyleUnmark}
-                        onMouseEnter={() => setIsHoveredUnmark(true)}
-                        onMouseLeave={() => setIsHoveredUnmark(false)}
-                    >Unmark Bee</li>
-                </ul>
-            )}
-        {
-        rowArray.map((r) => {
-            const className = Number(r) % 2 === 0 ? "evenRow" : "oddRow";
-            return (
-            <ul key={Number(r)} className={className}>
-                {
-                colArray.map((c) => {
-                    if (className === "evenRow") {
-                        if (Number(c) % 2 === 0) {
-                            return <li className="cellLi" key={uuid()}></li>
-                        } else {
-                            numCellsPlaced += 1;
-                            return <li 
-                                key={numCellsPlaced} 
-                                value={numCellsPlaced}
-                                className="cellLi"
-                                id={`${numCellsPlaced}`}
-                                >
-                                    <Cell 
-                                        cellInfo={cells.get(numCellsPlaced)} 
-                                        setCell={setCell} 
-                                        setGameOver={setGameOver} 
-                                        setGameWon={setGameWon}
-                                    />
-                                </li>
-                        }
-                    } else {
-                        if (Number(c) % 2 === 0) {
-                            numCellsPlaced += 1;
-                            return <li 
-                                key={numCellsPlaced}
-                                value={numCellsPlaced}
-                                className="cellLi"
-                                id={`${numCellsPlaced}`}
-                                >
-                                    <Cell 
-                                        cellInfo={cells.get(numCellsPlaced)} 
-                                        setCell={setCell} 
-                                        setGameOver={setGameOver} 
-                                        setGameWon={setGameWon}
-                                    />
-                                </li>
-                        } else {
-                            return <li className="cellLi" key={uuid()}></li>
-                        }
-                    }
-                })
+                  );
                 }
-            </ul>);
-        })
-        }
-        </div>
-    );
+              } else {
+                if (Number(c) % 2 === 0) {
+                  numCellsPlaced += 1;
+                  return (
+                    <li
+                      key={numCellsPlaced}
+                      value={numCellsPlaced}
+                      className="cellLi"
+                      id={`${numCellsPlaced}`}
+                    >
+                      <Cell
+                        cellInfo={cells.get(numCellsPlaced)}
+                        setCell={setCell}
+                        setGameOver={setGameOver}
+                        setGameWon={setGameWon}
+                      />
+                    </li>
+                  );
+                } else {
+                  return <li className="cellLi" key={uuid()}></li>;
+                }
+              }
+            })}
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
-export default CellsGrid;
+export default memo(CellsGrid);
